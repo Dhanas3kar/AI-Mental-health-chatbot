aa02c0141c8eed9e704c308cb7aee4a9a1a7ed43
dhanas3kar <dhanasekarmurgesan@gmail.com>
2025-07-01 15:05:28 +0530

    UI Completed

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,8 @@
 DB_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "Pheonix*0608",
-    "database": "AIchatbot"
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'Dhana@2005',
+    'database': 'AIchatbot'
 }
 
 GEMINI_API_KEY = "AIzaSyC0Z3dCIL6-PQa1x8cQTuRrtWyKR_FMLF4"  # 🔴 Replace with your actual Gemini API key
